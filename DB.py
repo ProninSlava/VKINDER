a9dc45b588b1c33d6032159a0dad67925d2ceabc
ProninSlava <99884823+ProninSlava@users.noreply.github.com>
2022-07-08 16:10:02 +0300

    Update DB.py

--- DB.py
+++ DB.py
@@ -10,7 +10,7 @@ from vk_info import user_token, group_token
 # Класс Base
 Base = declarative_base()
 
-DSN = 'postgresql://slava:7548@localhost:5432/music_bd'
+DSN = 'postgresql://slava:7548@localhost:5432/db_vk_person'
 
 # Функциональное использование ORM
 engine = sq.create_engine(DSN)
